a49d84bbc9c52741fff084791a1da8585e9786d2
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-05-08 00:22:46 +0600

    Add toggle option for company name settings
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- assets/src/js/admin.js
+++ assets/src/js/admin.js
@@ -1,24 +1,45 @@
-jQuery(function($){
-    $('body').on('click', '.wcpi_upload_image', function(e){
-        e.preventDefault();
+jQuery(function($) {
+  // Open wp image upload popup
+  $('body').on('click', '.wcpi_upload_image', function(e) {
+    e.preventDefault();
 
-        var button = $(this),
-        wcpi_uploader = wp.media({
-            title: 'Custom image',
-            library : {
-                uploadedTo : wp.media.view.settings.post.id,
-                type : 'image'
-            },
-            button: {
-                text: 'Use this image'
-            },
-            multiple: false
+    var button = $(this),
+      wcpi_uploader = wp
+        .media({
+          title: 'Custom image',
+          library: {
+            uploadedTo: wp.media.view.settings.post.id,
+            type: 'image',
+          },
+          button: {
+            text: 'Use this image',
+          },
+          multiple: false,
         })
         .on('select', function() {
-            var attachment = wcpi_uploader.state().get('selection').first().toJSON();
-            console.log(attachment.url);
-            $('#company_logo_upload-field').val(attachment.url);
+          var attachment = wcpi_uploader
+            .state()
+            .get('selection')
+            .first()
+            .toJSON();
+          console.log(attachment.url);
+          $('#company_logo_upload-field').val(attachment.url);
         })
         .open();
-    });
+  });
+
+  // Toggle Company Name text box
+  let company_name = $('#company_name_checkbox-field').is(':checked');
+  $('body').on('click', '#company_name_checkbox-field', function(e) {
+    console.log(company_name);
+    if (company_name === true) {
+      $('#company_name_text-wrapper').hide('slow');
+      company_name = false;
+    } else {
+      $('#company_name_text-wrapper').show('slow');
+      company_name = true;
+    }
+  });
+
+  console.log($('#company_name_checkbox-field').is(':checked'));
 });
