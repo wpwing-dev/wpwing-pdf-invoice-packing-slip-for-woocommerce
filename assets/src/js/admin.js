65fbc2773dc35ba38fedb9e7d2fb3129c766e959
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-05-08 17:38:36 +0600

    Implement show/hide in settings page
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- assets/src/js/admin.js
+++ assets/src/js/admin.js
@@ -29,22 +29,88 @@ jQuery(function($) {
   });
 
   // Toggle Company Name text box
-  let company_name = $('#company_name_checkbox-field').is(':checked');
-  if (company_name === true) {
+  let companyName = $('#company_name_checkbox-field').is(':checked');
+  if (companyName === true) {
     $('#company_name_text-wrapper').show();
   } else {
     $('#company_name_text-wrapper').hide();
   }
   $('body').on('click', '#company_name_checkbox-field', function(e) {
-    console.log(company_name);
-    if (company_name === true) {
+    if (companyName === true) {
       $('#company_name_text-wrapper').hide('slow');
-      company_name = false;
+      companyName = false;
     } else {
       $('#company_name_text-wrapper').show('slow');
-      company_name = true;
+      companyName = true;
     }
   });
 
-  console.log($('#company_name_checkbox-field').is(':checked'));
+  // Toggle Company Logo input box
+  let companyLogo = $('#company_logo_checkbox-field').is(':checked');
+  if (companyLogo === true) {
+    $('#company_logo_upload-wrapper').show();
+  } else {
+    $('#company_logo_upload-wrapper').hide();
+  }
+  $('body').on('click', '#company_logo_checkbox-field', function(e) {
+    if (companyLogo === true) {
+      $('#company_logo_upload-wrapper').hide('slow');
+      companyLogo = false;
+    } else {
+      $('#company_logo_upload-wrapper').show('slow');
+      companyLogo = true;
+    }
+  });
+
+  // Toggle Comapny Details input box
+  let companyDetails = $('#company_details_checkbox-field').is(':checked');
+  if (companyDetails === true) {
+    $('#company_details_text-wrapper').show();
+  } else {
+    $('#company_details_text-wrapper').hide();
+  }
+  $('body').on('click', '#company_details_checkbox-field', function(e) {
+    if (companyDetails === true) {
+      $('#company_details_text-wrapper').hide('slow');
+      companyDetails = false;
+    } else {
+      $('#company_details_text-wrapper').show('slow');
+      companyDetails = true;
+    }
+  });
+
+  // Toggle Comapny Notes input box
+  let companyNotes = $('#company_notes_checkbox-field').is(':checked');
+  if (companyNotes === true) {
+    $('#company_notes_text-wrapper').show();
+  } else {
+    $('#company_notes_text-wrapper').hide();
+  }
+  $('body').on('click', '#company_notes_checkbox-field', function(e) {
+    if (companyNotes === true) {
+      $('#company_notes_text-wrapper').hide('slow');
+      companyNotes = false;
+    } else {
+      $('#company_notes_text-wrapper').show('slow');
+      companyNotes = true;
+    }
+  });
+
+  // Toggle Comapny Footer input box
+  let companyFooter = $('#company_footer_checkbox-field').is(':checked');
+  if (companyFooter === true) {
+    $('#company_footer_text-wrapper').show();
+  } else {
+    $('#company_footer_text-wrapper').hide();
+  }
+  $('body').on('click', '#company_footer_checkbox-field', function(e) {
+    if (companyFooter === true) {
+      $('#company_footer_text-wrapper').hide('slow');
+      companyFooter = false;
+    } else {
+      $('#company_footer_text-wrapper').show('slow');
+      companyFooter = true;
+    }
+  });
+
 });
